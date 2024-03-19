601d0764e70b25e3fc60266030b61297914948e6
Kevin Quach <kevinquach@kevins-mbp.home>
2024-03-18 21:48:20 -0400

    fixed file pathing

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use('/api', api);
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.listen(port, () => {
